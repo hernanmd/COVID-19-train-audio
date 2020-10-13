13b9219047bebfd7ddab22de59f5fa89df12c285
cclauss <cclauss@me.com>
2020-10-13 08:57:41 +0200

    Make black, codespell, flake8, isort mandatory

--- feature extractor.py
+++ feature extractor.py
@@ -1,13 +1,13 @@
-#librosa feature extraction
+# librosa feature extraction
 import librosa
 import numpy as np
 
-#Run all the sound files get all the feature data and write it to
-#a csv file
+# Run all the sound files get all the feature data and write it to
+# a csv file
 
-audio = 'audio.wav' 
-y , sr = librosa.load(audio, mono=True, duration=1)
-#features
+audio = "audio.wav"
+y, sr = librosa.load(audio, mono=True, duration=1)
+# features
 chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
 spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
 spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
@@ -15,20 +15,7 @@ rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
 zcr = librosa.feature.zero_crossing_rate(y)
 mfcc = librosa.feature.mfcc(y=y, sr=sr)
 
-#printing the data
-print(np.mean(chroma_stft))
+# printing the data
+for feature in (chroma_stft, spec_cent, spec_bw, rolloff, zcr, mfcc):
+    print(np.mean(feature))
 
-
-print(np.mean(spec_cent))
-
-
-print(np.mean(spec_bw))
-
-
-print(np.mean(rolloff))
-
-
-print(np.mean(zcr))
-
-
-print(np.mean(mfcc))
